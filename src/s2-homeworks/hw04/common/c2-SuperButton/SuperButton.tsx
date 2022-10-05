470058a087b4d0f4736af1982576434236c23bc5
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2022-10-05 14:40:13 +0300

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,16 +17,36 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
+
+    // const getGeneralClass = (xType?: string, disabled?: boolean) => {
+    //     if(disabled) {
+    //         return s.disabled
+    //     }
+    //
+    //     if(xType === 'red') {
+    //         return s.red
+    //     }
+    //
+    //     if(xType === 'secondary') {
+    //         return s.secondary
+    //     }
+    //
+    //     return ''
+    // }
+    //
+    // const getFinalClass = (xType?: string, disabled?: boolean, className?: string) => {
+    //     const generalClass = getGeneralClass(xType, disabled)
+    //     return `${s.button} ${generalClass} ${className || ''}`
+    // }
+
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + ' ' + (disabled ? s.disabled : xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
             disabled={disabled}
+            // className={getFinalClass(xType, disabled, className)}
             className={finalClassName}
             {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
         />
