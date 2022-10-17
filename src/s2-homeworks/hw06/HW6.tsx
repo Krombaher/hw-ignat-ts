4b30d4357b6cc28c3f1e8fa5b31eb2beabaa74bb
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2022-10-17 23:47:12 +0300

    add local

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -13,7 +13,6 @@ import s from './HW6.module.css'
 
 const HW6 = () => {
     const [value, setValue] = useState<string>('')
-    console.log(value)
 
     const save = () => {
         saveState<string>('hw6-editable-span-value', value)
@@ -21,6 +20,7 @@ const HW6 = () => {
     const restore = () => {
         // делают студенты
         restoreState('hw6-editable-span-value', value)
+        setValue(restoreState('hw6-editable-span-value', value))
     }
 
     return (
